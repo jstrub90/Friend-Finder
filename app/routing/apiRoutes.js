6b04d83ebce67c6137eaabd1fb286aad84b86367
J.Strub <jstrub90@gmail.com>
2019-03-07 16:28:19 -0500

    set up server file

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+var friends = require('../data/friends.js');
+
+// setting all API routes here
+module.exports = function(app) {
+    // for a raw list of friends in JSON format
+    app.get('/api/friends', (req, res) => {
+        res.json(friends);
+    });
+
+    // for posting and checking friend match
+    app.post('/api/friends', (req, res) => {
+        // Here we take the result of the user's survey POST and parse it.
+        var userInput = req.body,
+            userName = userInput.name,
+            userAnswers = userInput.scores,
+            answersArr = [], // this will store all the friend's score comparison, lowest score is the match
+            answersDiff = 0; // this will store the current friend's total score
+
+            console.log(userAnswers);
+
+        friends.forEach( (item, index) => {
+            let scores = item.scores; // scores of the current potential fiend
+
+            scores.forEach( (item, index) => {
+                let questionDiff = Math.abs(parseInt(item) - parseInt(userAnswers[index])); // diff for current question
+                answersDiff += questionDiff; // adding the current question's diff to the total diff
+                // console.log(scoreDiff);
+            });
+            answersArr.push(scoreDiff);
+            answersDiff = 0;
+        });
+
+        var lowScore = Math.min(...answersArr), // determine the lowest score in the array
+            indexOfLowScore = scoreArr.indexOf(lowScore), // determine the index of first lowest score
+            match = friends[indexOfLowScore];
+
+        // adding new person to the friends list array
+        friends.push(userInput);
+
+        // sending back the friend match
+        res.json(match);
+    });
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+    path = require("path");
+
+// setup the express app
+var app = express(),
+    PORT = process.env.PORT || 3000;
+
+// setup the express app to handle data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./app/public')); // this is the server's root folder
+
+// app router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// starts the server and begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
